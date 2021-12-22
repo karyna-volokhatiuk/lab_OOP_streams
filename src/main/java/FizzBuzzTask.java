3d802706f669a4cfde819926c46237a6ad94cd8a
Karyna Volokhatiuk <karyna.volokhatiuk@ucu.edu.ua>
2021-12-22 23:01:38 +0200

    tuz

---
+++ src/main/java/FizzBuzzTask.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class FizzBuzzTask {
+    public static List<Integer> makeList(int n) {
+        return IntStream.range(1, n + 1).boxed().collect(Collectors.toList());
+    }
+
+    public static List<String> fizzBuzzIt(List<Integer> intArr) {
+        return intArr.stream().map(x -> {
+            if (x % 15 == 0) {
+                return "Fizz Buzz";
+            } else if (x % 3 == 0) {
+                return "Fizz";
+            } else if (x % 5 == 0) {
+                return "Buzz";
+            }
+            return x.toString();
+        }).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println(MapTask.mapping(Arrays.asList("1", "2", "str", "3")));
+        System.out.println(FlattingTask.flattingStrings(Arrays.asList("abc", "def", "ghj")));
+        System.out.println(FizzBuzzTask.fizzBuzzIt(FizzBuzzTask.makeList(15)));
+    }
+}
